fc03cb4b4701f253a2506286046db5530108e6e1
jhaabhishek8076 <jhaabhishek8076@gmail.com>
2022-07-08 12:02:54 +0530

    push

--- src/form.js
+++ src/form.js
@@ -1,64 +1,146 @@
-import React,{useState} from "react";
-import "./App.css";
-const Form = () => {
-    const [name,setname]=useState({});
-  const  handlechange=(e)=>{
-  
-setname(values=>({...values,[e.target.name]:e.target.value}));
-//const [name,setname]=useState("");
-console.log(name)
-    }
-    return (
-        <>
-        <div>
-             <input  id="fname" name="FirstName" value={name.FirstName||""} type="text" placeholder="FirstName" onChange={(e)=>handlechange(e)}></input>
-             <br/>
-             <input class="Lbtn" name="LastName"  value={name.LastName|| ""} type="text" placeholder="LastName" onChange={(e)=>handlechange(e)}></input>
-             <br/>
-             <button>Submit</button>
-             
-        </div>
-        </>
-    );
-  
-}
-// function Form() {
-//     const [inputs, setInputs] = useState({});
-  
-//     const handleChange = (event) => {
-//       const name = event.target.name;
-//       const value = event.target.value;
-//       setInputs(values => ({...values, [name]: value}))
-//       console.log(inputs)
-//     }
-  
-//     const handleSubmit = (event) => {
-//       event.preventDefault();
-//       alert(inputs);
-//     }
-  
-//     return (
-//       <form onSubmit={handleSubmit}>
-//         <label>Enter your name:
-//         <input 
-//           type="text" 
-//           name="username" 
-//           value={inputs.username || ""} 
-//           onChange={handleChange}
-//         />
-//         </label>
-//         <label>Enter your age:
-//           <input 
-//             type="number" 
-//             name="age" 
-//             value={inputs.age || ""} 
-//             onChange={handleChange}
-//           />
-//           </label>
-//           <input type="submit" />
-//       </form>
-//     )
-//   }
-  
+import React, { useState, useEffect } from "react";
 
-export default Form;
+import "./App.css";
+import { Button, Form, FormGroup, Label, Col, Row, Input, DropdownToggle, DropdownMenu, FormText, Container, UncontrolledPopover, Select, Dropdown, DropdownItem } from 'reactstrap';
+const styles = {
+  col: {
+    "background": "#627799",
+    "color": "red",
+    "borderRadius": "50px",
+
+  }
+}
+//{}  let aa=['s','erre']
+const Form1 = () => {
+  const [input, setInput] = useState([{
+    fname: "",
+    lname: "",
+    address: "",
+    phonecode: "",
+    phone: ""
+  }]);
+
+  const [validata, setvalidata] = useState({
+    fname: "",
+    lname: "",
+    address: "",
+    phonecode: "",
+    phone: ""
+  }
+  );
+
+  React.useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(results => results.json())
+      .then(data => {
+        const { name } = data.results[0];
+        // setFirstName(name.first);
+        // setLastName(name.last);
+        console.log(name);
+      });
+  }, []); // <-- Have to pass in [] here!
+
+  const onsubmit = (e) => {
+    let newfield = {
+      fname: "",
+      lname: "",
+      address: "",
+      phonecode: "",
+      phone: ""
+    }
+
+    setInput([...input, newfield])
+    // e.preventDefault();
+    // let errors={...validata};
+
+    // if (input.fname==="") {
+    //   errors.fname = "enter first name"; 
+    // }
+    // else{
+    //   errors.fname=" ";
+    // }
+    // if(input.lname===""){
+    //   errors.lname="enter your last name";
+    // }
+    // else{
+    //   errors.lname=" ";
+    // }
+    // setvalidata(errors);
+
+  }
+  const handlechange = (event, index) => {
+    let data = [...input];
+    data[index][event.target.name] = event.target.value;
+    setInput(data);
+    //setInput(values=>({...values,[e.target.name]:e.target.value}));
+    // const [name,setname]=useState("");
+    console.log(input)
+  }
+  return (
+
+    <>
+      <h1 style={{ "textAlign": "center" }}><font size="32" >Form</font></h1>
+      {input.map((value, index) => {
+        return <Container key={index} style={{ "border": '1px solid', "background": "#ebe134" }}>
+
+          <Row>
+            <Col xs="4" style={styles.col}><Label>First Name</Label></Col>
+            <Col xs="8"  ><Input style={{ "borderRadius": "50px" }} type="text" name="fname" value={value.fname} placeholder="First Name" onChange={(e) => { handlechange(e, index) }}></Input>
+
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="4"></Col>
+            <Col xs="8">
+              {validata.fname && <p>{validata.fname}</p>}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="4" style={styles.col}><Label>Last Name</Label></Col>
+            <Col xs="8" ><Input type="text" placeholder="Last Name" name="lname" onChange={handlechange}></Input>{validata.lname && <p>{validata.lname}</p>}</Col>
+
+          </Row>
+          <Row>
+            <Col xs="4" style={styles.col}><Label>Address</Label></Col>
+            <Col xs="8" ><Input type="text" name="address" onChange={handlechange}></Input></Col>
+          </Row>
+          <Row>
+            <Col xs="2" style={styles.col}><Label>phone no</Label></Col>
+            <Col xs="2" ><Input type="number" placeholder="+91" name="phonecode" onChange={handlechange}></Input></Col>
+            <Col xs="8"><Input type="number" name="phone" onChange={handlechange}></Input></Col>
+          </Row>
+          <Row>
+            <Col xs="4" style={styles.col}><Label >Email</Label></Col>
+            <Col xs="8">
+              <Input type="email" name="Email"></Input>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="2" style={styles.col}><Label name="country">Country</Label>
+            </Col>
+
+            <Col xs="4">
+              <select className="form-control">
+                <option>Choose</option>
+                <option value="Afghanistan">Afghanistan</option>
+                <option value="Albania">Albania</option>
+                <option value="Algeria">Algeria</option>
+                <option value="American Samoa">American Samoa</option>
+              </select>
+            </Col>
+          </Row>
+          <Row>
+          </Row>
+        </Container>
+      })}
+      <Row>
+        <Col xs="2" >
+          <Button onClick={onsubmit}>Add more</Button>
+        </Col>
+      </Row>
+    </>
+  );
+
+}
+
+export default Form1;
